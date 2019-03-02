3cf279846ea69ab0dae6019fa656d636e0c0f8ea
Unknown <the.nightling@gmail.com>
2019-03-02 17:13:17 +0200

    Add Selection Sort

---
+++ SortAlgorithms/SelectionSort.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfSortApplication.SortAlgorithms
+{
+	public class SelectionSort : SortAlgorithm
+	{
+		public SelectionSort(ObservableCollection<Item> items)
+			: base(items)
+		{ }
+
+		public override void Sort()
+		{
+			for (int i = 0; i < Items.Count - 1; i++)
+				SelectionSortStep(i);
+		}
+
+		public override void SortStep()
+		{
+			if (this.currentStep >= Items.Count - 1)
+				this.currentStep = 0;
+
+			SelectionSortStep(this.currentStep);
+
+			this.currentStep++;
+		}
+
+		private void SelectionSortStep(int currentIndex)
+		{
+			int minIndex = FindMinIndex(currentIndex);
+
+			if (minIndex != currentIndex)
+			{
+				Item tempItem = Items[currentIndex];
+				Items[currentIndex] = Items[minIndex];
+				Items[minIndex] = tempItem;
+			}
+		}
+
+		int FindMinIndex(int arrayStartIndex)
+		{
+			int minIndex = arrayStartIndex;
+			for (int i = arrayStartIndex + 1; i < Items.Count; i++)
+			{
+				if (Items[i].Value < Items[minIndex].Value)
+					minIndex = i;
+			}
+
+			return minIndex;
+		}
+	}
+}
---
+++ SortAlgorithms/SortAlgorithm.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfSortApplication
+{
+	public abstract class SortAlgorithm
+	{
+		protected int currentStep = 0;
+
+		public SortAlgorithm(ObservableCollection<Item> items)
+		{
+			Items = items;
+		}
+
+		protected ObservableCollection<Item> Items { get; }
+
+		public abstract void Sort();
+
+		public abstract void SortStep();
+	}
+}
