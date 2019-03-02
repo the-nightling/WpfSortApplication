f924464103f5b16a2dcd92903bab202d0f4de79a
Unknown <the.nightling@gmail.com>
2019-03-02 13:09:02 +0200

    Add UI

---
+++ RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+	public class RelayCommand : ICommand
+	{
+		private readonly Action<object> action;
+
+		public RelayCommand(Action<object> action)
+		{
+			this.action = action;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+
+		public void Execute(object parameter)
+		{
+			this.action(parameter);
+		}
+
+		public event EventHandler CanExecuteChanged;
+	}
+}
